da451f8804319e3d57f1a3baf23121ced7193c0a
Anthony Monori <anthonymonori@gmail.com>
2012-10-03 00:10:00 +0200

    test

---
+++ src/word/guesser/WordGuesser.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package word.guesser;
+//this is a test
+/**
+ *
+ * @author Htx-Bruger
+ */
+public class WordGuesser
+{
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {    
+      GameConsole gc = new GameConsole(); 
+      gc.play();
+        
+    }
+    
+}
+
