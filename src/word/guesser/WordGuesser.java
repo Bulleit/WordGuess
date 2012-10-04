e8033ae4fa54fe4ae3b2e6ca420382dbf64e9972
Anthony Monori <anthonymonori@gmail.com>
2012-10-04 11:22:43 +0200

    This class only initializes the game.

--- src/word/guesser/WordGuesser.java
+++ src/word/guesser/WordGuesser.java
@@ -3,24 +3,20 @@
  * and open the template in the editor.
  */
 package word.guesser;
-//this is a test
+
 /**
  *
  * @author Htx-Bruger
  */
-public class WordGuesser
-{
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {    
-      GameConsole gc = new GameConsole(); 
-      gc.play();
-        
-    }
-    
-}
+public class WordGuesser {
 
+    /**
+     * This is the main object that runs the game
+     *
+     * @param args the command line arguments.
+     */
+    public static void main(String[] args) {
+        GameConsole gc = new GameConsole();
+        gc.play();
+    }
+}
